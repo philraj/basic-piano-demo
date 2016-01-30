135f5f6c023711ff05ae6276096af614965316a7
Phil Rajchgot <tophil@outlook.com>
2016-01-30 21:27:22 +0000

    Linked to Howler.js, added 3 audio files that play when click is fired from the 3 buttons

--- piano.js
+++ piano.js
@@ -1,5 +1,27 @@
+var hammer1 = new Howl({
+    urls: ["[HammerPerx] 03.wav"]
+})
+
+var hammer2 = new Howl({
+    urls: ["[HammerPerx] 07.wav"]
+})
+
+var goat = new Howl({
+    urls: ["[man-goat]01.wav"]
+})
+
 $(document).ready( function () {
     $("button").click( function (e) {
-        alert(e.target.innerHTML);
+        switch (e.target.id) {
+            case 'button1':
+                hammer1.play();
+                break;
+            case 'button2':
+                hammer2.play();
+                break;
+            case 'button3':
+                goat.play();
+                break;
+        }
     })
 })
